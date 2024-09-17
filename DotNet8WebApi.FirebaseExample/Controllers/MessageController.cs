f613c913e52935efc7741f79a0e38937497dd56b
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-17 23:40:09 +0630

    Add project files.

---
+++ DotNet8WebApi.FirebaseExample/Controllers/MessageController.cs
@@ -0,0 +1,36 @@
+﻿using DotNet8WebApi.FirebaseExample.Models;
+using FirebaseAdmin.Messaging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8WebApi.FirebaseExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> SendMessageAsync([FromBody] MessageRequest messageRequest)
+        {
+            var message = new Message()
+            {
+                Notification = new Notification
+                {
+                    Title = messageRequest.Title,
+                    Body = messageRequest.Body,
+                },
+                Data = new Dictionary<string, string>()
+                {
+                    ["FirstName"] = "Lin Thit",
+                    ["LastName"] = "Htoo"
+                },
+                Token = messageRequest.DeviceToken
+            };
+
+            var messaging = FirebaseMessaging.DefaultInstance;
+            var result = await messaging.SendAsync(message);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ DotNet8WebApi.FirebaseExample/Models/MessageRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNet8WebApi.FirebaseExample.Models
+{
+    public class MessageRequest
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string DeviceToken { get; set; }
+    }
+}
