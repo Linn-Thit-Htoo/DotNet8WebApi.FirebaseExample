ef47a61474737a967dbeb460b004e0853ebf890a
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-25 16:00:10 +0630

    -

--- DotNet8WebApi.FirebaseExample/GlobalUsings.cs
+++ DotNet8WebApi.FirebaseExample/GlobalUsings.cs
@@ -1,3 +1,5 @@
 ﻿global using DotNet8WebApi.FirebaseExample.Models;
 global using FirebaseAdmin.Messaging;
 global using Microsoft.AspNetCore.Mvc;
+global using FirebaseAdmin;
+global using Google.Apis.Auth.OAuth2;
--- DotNet8WebApi.FirebaseExample/Program.cs
+++ DotNet8WebApi.FirebaseExample/Program.cs
@@ -1,23 +1,23 @@
-global using FirebaseAdmin;
-global using Google.Apis.Auth.OAuth2;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-FirebaseApp.Create(new AppOptions()
-{
-    Credential = GoogleCredential.FromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "testfirebase-ad8e7-firebase-adminsdk-9e77o-4079fd9b62.json")),
-});
+FirebaseApp.Create(
+    new AppOptions()
+    {
+        Credential = GoogleCredential.FromFile(
+            Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "testfirebase-ad8e7-firebase-adminsdk-9e77o-4079fd9b62.json"
+            )
+        ),
+    }
+);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
