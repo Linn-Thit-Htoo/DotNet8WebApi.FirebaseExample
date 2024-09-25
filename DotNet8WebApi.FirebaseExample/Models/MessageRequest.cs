e833f0f522b38f125ddc00980bfefe12ce70bde5
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-25 15:59:44 +0630

    -

--- DotNet8WebApi.FirebaseExample/Models/MessageRequest.cs
+++ DotNet8WebApi.FirebaseExample/Models/MessageRequest.cs
@@ -1,9 +1,8 @@
-﻿namespace DotNet8WebApi.FirebaseExample.Models
+﻿namespace DotNet8WebApi.FirebaseExample.Models;
+
+public class MessageRequest
 {
-    public class MessageRequest
-    {
-        public string Title { get; set; }
-        public string Body { get; set; }
-        public string DeviceToken { get; set; }
-    }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public string DeviceToken { get; set; }
 }
